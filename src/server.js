ddc34d931f651f59376c1f75271a10c3b6ef747f
tonitone <030toni@gmail.com>
2021-10-20 20:33:19 +0200

    feat: add swagger and configuration yml

--- src/server.js
+++ src/server.js
@@ -1,7 +1,18 @@
+import fs from 'fs'
+import YAML from 'yaml'
 import express from 'express'
+import swaggerUi from 'swagger-ui-express'
+const swaggerDocument = YAML.parse(
+  fs.readFileSync('./swagger.yml', 'utf8')
+)
+
 const app = express()
+
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+
+// swagger stuff
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
 
 app.get('/', (req, res) => {
   return res.status(200).send({
