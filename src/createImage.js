562dfaeaccee2ceb611dbb5ad8a50fa4723e0399
tonitone <030toni@gmail.com>
2021-10-21 04:20:06 +0200

    feat: add image generation over express.
    Problem is, that the Promise is not returning. Without the Promise block, Express sendFile is returning the image faster, than it is genrated

--- src/createImage.js
+++ src/createImage.js
@@ -1,26 +1,40 @@
 import Jimp from 'jimp'
+import fs from 'fs'
+
+const storePath = './public/image-store'
+let imagePath = ''
+
+export const getImagePath = _ => {
+  return imagePath
+}
 
 export const createImage = parameters => {
-  const publicPath = './public/'
-  const image = new Jimp(
-    parameters.dimension.width,
-    parameters.dimension.height,
-    parameters.backgroundColor,
-    (err, image) => {
-      if (err) throw err
-    })
+  return new Promise(function (resolve, reject) {
+    const fileName = `${parameters.dimension.width}_${parameters.dimension.height}_${parameters.backgroundColor}`
+    const message = `${parameters.dimension.width} X ${parameters.dimension.height}`
+    const x = 10
+    const y = 10
 
-  const fileName = `${parameters.dimension.width}_${parameters.dimension.height}_${parameters.backgroundColor}`
-  const message = `${parameters.dimension.width} X ${parameters.dimension.height}`
-  const x = 10
-  const y = 10
+    parameters.backgroundColor = parameters.backgroundColor || '000000ff'
+    imagePath = `${storePath}/${fileName}.${parameters.extension}`
 
-  Jimp.loadFont(Jimp.FONT_SANS_14_BLACK)
-    .then(font => {
-      image.print(font, x, y, message)
-      return image
-    }).then(image => {
-      const file = `${publicPath}/${fileName}.${image.getExtension()}`
-      return image.write(file) // save
-    })
+    if (fs.existsSync(getImagePath())) {
+      return true
+    }
+    const image = new Jimp(
+      parameters.dimension.width,
+      parameters.dimension.height,
+      parameters.backgroundColor,
+      (err, image) => {
+        if (err) throw err
+      })
+
+    Jimp.loadFont(Jimp.FONT_SANS_14_BLACK)
+      .then(font => {
+        image.print(font, x, y, message)
+        return image
+      }).then(image => {
+        return image.write(getImagePath()) // save
+      })
+  })
 }
--- src/server.js
+++ src/server.js
@@ -1,7 +1,10 @@
 import fs from 'fs'
+import path from 'path'
 import YAML from 'yaml'
 import express from 'express'
+import mime from 'mime-types'
 import swaggerUi from 'swagger-ui-express'
+import { createImage, getImagePath } from './createImage.js'
 const swaggerDocument = YAML.parse(
   fs.readFileSync('./swagger.yml', 'utf8')
 )
@@ -10,6 +13,9 @@ const app = express()
 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+
+// public folder
+app.use(express.static('../public'))
 
 // swagger stuff
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
@@ -21,4 +27,34 @@ app.get('/', (req, res) => {
   })
 })
 
-app.listen(6565)
+app.get('/:extension/:dimension', async (req, res, next) => {
+  const options = { }
+  const dimension = req.params.dimension.split('x')
+  const fileName = path.resolve(path.resolve(getImagePath()))
+  await createImage({
+    extension: req.params.extension,
+    dimension: {
+      width: dimension[0],
+      height: dimension[1]
+    }
+  })
+  res
+    .status(200)
+    .contentType(mime.lookup(req.params.extension))
+    .sendFile(
+    // path.resolve(getImagePath()),
+      fileName,
+      // { root: 'image-store' }
+      options,
+      function (err) {
+        if (err) {
+          next(err)
+        } else {
+          console.log('Sent:', fileName)
+        }
+      }
+    )
+  res.end(fileName, 'binary')
+})
+
+app.listen(8000)
